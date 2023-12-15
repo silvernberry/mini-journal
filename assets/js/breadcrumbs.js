01cce4f2cfa8fe1024c5618f7b74a37de67f2c76
silvernberry <silverberry622@gmail.com>
2023-12-15 23:09:27 +0530

    Features update v3

--- assets/js/breadcrumbs.js
+++ assets/js/breadcrumbs.js
@@ -1,35 +1,68 @@
-  // Function to update breadcrumbs based on scroll position
-  function updateBreadcrumbs() {
-    const headings = document.querySelectorAll('#journal h1, #journal h2, #journal h3, #journal h4, #journal h5, #journal h6');
-    const breadcrumbs = document.getElementById('breadcrumbs');
+// Function to update breadcrumbs based on scroll position
+function updateBreadcrumbs() {
+  const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  const breadcrumbs = document.getElementById('breadcrumbs');
 
-    let visibleHeadings = [];
+  let visibleHeadings = [];
+  let foundFirstH1 = false;
 
-    headings.forEach(heading => {
-      const rect = heading.getBoundingClientRect();
+  headings.forEach(heading => {
+    const rect = heading.getBoundingClientRect();
 
-      if (rect.top < window.innerHeight / 2) {
-        visibleHeadings.push(heading.textContent);
+    if (rect.top < window.innerHeight / 2) {
+      if (heading.tagName === 'H1') {
+        foundFirstH1 = true;
+        visibleHeadings = []; // Clear previous headings when H1 is found
+      }
+
+      if (foundFirstH1) {
+        const level = parseInt(heading.tagName.charAt(1));
+        visibleHeadings = visibleHeadings.filter(item => item.level < level);
+        const headingText = heading.textContent.replace(/#/g, '').trim(); // Remove trailing "#" and trim
+        const headingId = heading.id || headingText.replace(/\s+/g, '-').toLowerCase();
+        visibleHeadings.push({ level, text: headingText, id: headingId });
+      }
+    }
+  });
+
+  // Sort visibleHeadings by level
+  visibleHeadings.sort((a, b) => a.level - b.level);
+
+  breadcrumbs.innerHTML = ''; // Clear existing breadcrumbs
+
+  for (let i = 0; i < visibleHeadings.length; i++) {
+    const headingText = visibleHeadings[i].text;
+    const headingId = visibleHeadings[i].id;
+
+    const link = document.createElement('a');
+    link.href = `#${headingId}`;
+    link.textContent = headingText;
+
+    // Add click event listener to each breadcrumb link
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      // Scroll to the corresponding heading with an offset of 50 pixels
+      const targetHeading = document.getElementById(headingId);
+      if (targetHeading) {
+        const offset = targetHeading.getBoundingClientRect().top + window.scrollY - 50;
+        window.scrollTo({ top: offset, behavior: 'smooth' });
       }
     });
 
-    const start = Math.max(visibleHeadings.length - 6, 0);
+    const breadcrumbItem = document.createElement('div');
+    breadcrumbItem.classList.add('breadcrumb-item');
+    breadcrumbItem.appendChild(link);
 
-    breadcrumbs.innerHTML = ''; // Clear existing breadcrumbs
-
-    for (let i = start; i < visibleHeadings.length; i++) {
-      const breadcrumbItem = document.createElement('div');
-      breadcrumbItem.classList.add('breadcrumb-item');
-      breadcrumbItem.textContent = visibleHeadings[i];
-      breadcrumbs.appendChild(breadcrumbItem);
-    }
-
-    // Toggle breadcrumbs visibility based on scroll position
-    breadcrumbs.style.display = window.scrollY > 50 ? 'block' : 'none';
+    breadcrumbs.appendChild(breadcrumbItem);
   }
 
-  // Update breadcrumbs on scroll
-  window.addEventListener('scroll', updateBreadcrumbs);
+  // Toggle breadcrumbs visibility based on scroll position
+  breadcrumbs.style.display = window.scrollY > 50 ? 'block' : 'none';
+}
 
-  // Initial update on page load
-  window.addEventListener('DOMContentLoaded', updateBreadcrumbs);
+// Update breadcrumbs on scroll
+window.addEventListener('scroll', updateBreadcrumbs);
+
+// Initial update on page load
+window.addEventListener('DOMContentLoaded', updateBreadcrumbs);
